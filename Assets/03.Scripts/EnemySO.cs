efda7b44dcefa5896ebf2eb25ab09d187729e27a
ggach1 <dlsdnj1234@gmail.com>
2024-09-19 18:15:59 +0900

    enemy so % panel fixed

--- Assets/03.Scripts/EnemySO.cs
+++ Assets/03.Scripts/EnemySO.cs
@@ -6,5 +6,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "EnemySO", menuName = "Scriptable Object/Enemy")]
 public class EnemySO : ScriptableObject
 {
-    
+    public string _enemyName;
+    public float _attack;
+    public float _health;
+    public float _evasion;
 }
